2d6ea72016b5bd57e6ecaa364526fed16883c5bf
suochenfeng <suochenfeng@ky-tech.com.cn>
2019-06-09 16:25:12 +0800

    搭建基本的博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,9 +10,9 @@ module.exports = {
     },
     themeConfig: {
       nav:[ // 导航栏配置
-        {text: '前端基础', link: '/accumulate/' },
-        {text: '算法题库', link: '/algorithm/'},
-        {text: '微博', link: 'https://baidu.com'}      
+        {text: '主页', link: '/' },
+        {text: '前端', link: '/font/'},
+        {text: 'github', link: 'https://github.com/soyomo'}      
       ],
       sidebar: 'auto', // 侧边栏配置
       sidebarDepth: 2, // 侧边栏显示2级
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,16 @@
-基于vuepress 的个人博客
+---
+home: true
+heroImage: /logo.png
+actionText: 快速上手 →
+actionLink: /zh/guide/
+features:
+- title: 前端三驾马车
+  details: Vue、React、Abgular的学习和实践。
+- title: 工作问题及解决
+  details: 记录平时工作中遇到的一些问题和解决方法。
+- title: 前端可视化
+  details: 前端方面可视化的知识，包括webgl,canvas,glsl等。
+- title: 后端的学习
+details: 一些个人对后端的学习和实践。
+footer: MIT Licensed | Copyright © 2019-present chenfeng
+---

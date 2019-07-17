90ab2b5f71bcfe4b773aed3190501b41243525a9
suochenfeng <suochenfeng@ky-tech.com.cn>
2019-07-17 15:51:58 +0800

    add

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 git push -f https://${token}@${address} master:master
 git push -f https://${chenfeng_token}@${chenfeng_address} master:master
 # git push -f git@github.com:soyomo/soyomo.github.io.git master
-
+# adsfasd
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
